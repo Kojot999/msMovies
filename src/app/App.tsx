636bfb08734537a3615a720b2f5acd2d7d51a4ec
Kojot999 <piotr-ankus@wp.pl>
2022-11-30 15:51:04 +0100

    feat: layout prep

---
+++ src/app/App.tsx
@@ -0,0 +1,17 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Container">
+        <div className="Wrapper">
+          <div className="Header">Header</div>
+          <div className="Content">Content</div>
+        </div>
+      </div>
+      <div className="Footer">Fotter</div>
+    </div>
+  );
+}
+
+export default App;
